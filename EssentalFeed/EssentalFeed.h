c28db01d98fbd24c542b1ef20a48c601b0717221
Betzer09 <betzer.austin09@gmail.com>
2022-02-09 08:30:28 -0700

    Initial Commit

---
+++ EssentalFeed/EssentalFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentalFeed.h
+//  EssentalFeed
+//
+//  Created by Austin Betzer on 2/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentalFeed.
+FOUNDATION_EXPORT double EssentalFeedVersionNumber;
+
+//! Project version string for EssentalFeed.
+FOUNDATION_EXPORT const unsigned char EssentalFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentalFeed/PublicHeader.h>
+
+
---
+++ EssentalFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentalFeed
+//
+//  Created by Austin Betzer on 2/9/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
