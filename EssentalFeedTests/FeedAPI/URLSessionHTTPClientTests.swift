a13b26586723ee89bc832115dcbbb86c4b6e5cf5
Betzer09 <betzer.austin09@gmail.com>
2022-03-01 08:19:50 -0700

    Extracted tests values creation to shared factory methods

--- EssentalFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentalFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -58,7 +58,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let requestError = NSError(domain: "any error", code: 1)
+        let requestError = anyNSError()
         let receivedError = resultErrorFor(data: nil, response: nil, error: requestError)
         
     
@@ -67,20 +67,16 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnAllNilValues() {
-        let anyData = Data(bytes: "any data".utf8)
-        let anyError = NSError(domain: "any erro", code: 0)
-        let nonHTTPURLResponse = URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
-        let anyHTTPURLResponse = HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
         XCTAssertNotNil(resultErrorFor(data:nil, response:nil, error:nil))
-        XCTAssertNotNil(resultErrorFor(data:nil, response:nonHTTPURLResponse, error:nil))
-        XCTAssertNotNil(resultErrorFor(data:nil, response:anyHTTPURLResponse, error:nil))
-        XCTAssertNotNil(resultErrorFor(data:anyData, response:nil, error:nil))
-        XCTAssertNotNil(resultErrorFor(data:anyData, response:nil, error:anyError))
-        XCTAssertNotNil(resultErrorFor(data:nil, response:nonHTTPURLResponse, error:anyError))
-        XCTAssertNotNil(resultErrorFor(data:nil, response:anyHTTPURLResponse, error:anyError))
-        XCTAssertNotNil(resultErrorFor(data:anyData, response:nonHTTPURLResponse, error:anyError))
-        XCTAssertNotNil(resultErrorFor(data:anyData, response:anyHTTPURLResponse, error:anyError))
-        XCTAssertNotNil(resultErrorFor(data:anyData, response:nonHTTPURLResponse, error:nil))
+        XCTAssertNotNil(resultErrorFor(data:nil, response:nonHTTPURLResponse(), error:nil))
+        XCTAssertNotNil(resultErrorFor(data:nil, response:anyHTTPURLResponse(), error:nil))
+        XCTAssertNotNil(resultErrorFor(data:anyData(), response:nil, error:nil))
+        XCTAssertNotNil(resultErrorFor(data:anyData(), response:nil, error:anyNSError()))
+        XCTAssertNotNil(resultErrorFor(data:nil, response:nonHTTPURLResponse(), error:anyNSError()))
+        XCTAssertNotNil(resultErrorFor(data:nil, response:anyHTTPURLResponse(), error:anyNSError()))
+        XCTAssertNotNil(resultErrorFor(data:anyData(), response:nonHTTPURLResponse(), error:anyNSError()))
+        XCTAssertNotNil(resultErrorFor(data:anyData(), response:anyHTTPURLResponse(), error:anyNSError()))
+        XCTAssertNotNil(resultErrorFor(data:anyData(), response:nonHTTPURLResponse(), error:nil))
         
     }
     
@@ -90,6 +86,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         trackForMemoryLeak(sut, file: file, line: line)
         return sut
     }
+    
     
     private func resultErrorFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath, line: UInt = #line) -> Error? {
         URLProtocolStub.stub(data: data, response: response, error: error)
@@ -115,6 +112,22 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     private func anyURL() -> URL {
         return URL(string: "http://any-url.com")!
+    }
+    
+    private func anyData() -> Data {
+        return Data(bytes: "any data".utf8)
+    }
+    
+    private func nonHTTPURLResponse() -> URLResponse {
+        URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+    }
+    
+    private func anyHTTPURLResponse() -> HTTPURLResponse {
+        HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)!
+    }
+    
+    private func anyNSError() -> NSError {
+        NSError(domain: "any erro", code: 0)
     }
     
     private class URLProtocolStub: URLProtocol {
