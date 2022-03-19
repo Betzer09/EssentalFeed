856f51bc3a9d74efe2d710c095dcbae2e43856e0
Betzer09 <betzer.austin09@gmail.com>
2022-03-19 09:28:37 -0600

    Move FeedStore to its own file

---
+++ EssentalFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentalFeed
+//
+//  Created by Austin Betzer on 3/19/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+}
