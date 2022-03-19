408a242cb92f3da0339b996006315cf070be3b32
Betzer09 <betzer.austin09@gmail.com>
2022-03-19 10:13:16 -0600

    Moved 'LocalFeedItem' to its own file

---
+++ EssentalFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,26 @@
+//
+//  LocalFeedItem.swift
+//  EssentalFeed
+//
+//  Created by Austin Betzer on 3/19/22.
+//
+
+import Foundation
+
+// Data Transfer Object Pattern
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID,
+                description: String?,
+                location: String?,
+                imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
