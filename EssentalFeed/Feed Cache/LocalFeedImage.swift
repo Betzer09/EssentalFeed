d1a3787daefd31d15995f639a6a0c9e03abd7b53
Betzer09 <betzer.austin09@gmail.com>
2022-04-05 11:14:13 -0600

    Move codable conformance from the framework-agnostic 'LocalFeedImage' type to the new framework-specific 'CodableFeedImage' type. The 'CodableFeedImage' is a private type within the framework implementation since the 'Codable' requirement is a framework-specific detail.

--- EssentalFeed/Feed Cache/LocalFeedImage.swift
+++ EssentalFeed/Feed Cache/LocalFeedImage.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // Data Transfer Object Pattern
-public struct LocalFeedImage: Equatable, Codable {
+public struct LocalFeedImage: Equatable {
     public let id: UUID
     public let description: String?
     public let location: String?
