e3c77aa78595e5955bba3a30e94f95bb4a28bdd0
Betzer09 <betzer.austin09@gmail.com>
2022-04-07 10:23:54 -0600

    Proved that the 'CodableFeedStore' side-effects run serially

--- EssentalFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentalFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -125,6 +125,33 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .empty)
     }
     
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        var completedOperationsInOrder = [XCTestExpectation]()
+        
+        let op1 = expectation(description: "operation 1")
+        sut.insert(uniqueImageFeed().local, timestamp: Date(), completion: {_ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        })
+        
+        let op2 = expectation(description: "operation 2")
+        sut.deleteCachedFeed(completion: {_ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        })
+        
+        let op3 = expectation(description: "operation 3")
+        sut.insert(uniqueImageFeed().local, timestamp: Date(), completion: {_ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        })
+        
+        waitForExpectations(timeout: 5.0)
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3])
+        
+    }
+    
     // MARK: - Helpers
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
         let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
