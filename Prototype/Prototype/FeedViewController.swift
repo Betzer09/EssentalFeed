dd9f4623752effdf1cb119f94282cc7244a50a4d
Betzer09 <betzer.austin09@gmail.com>
2022-06-11 10:26:22 -0600

    Added shimmering to load images

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -19,22 +19,22 @@ final class FeedViewController: UITableViewController {
     private var feed = [FeedImageViewModel]()
     
     override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-
-            refresh()
-            tableView.setContentOffset(CGPoint(x: 0, y: -tableView.contentInset.top), animated: false)
-        }
-
-        @IBAction func refresh() {
-            refreshControl?.beginRefreshing()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                if self.feed.isEmpty {
-                    self.feed = FeedImageViewModel.prototypeFeed
-                    self.tableView.reloadData()
-                }
-                self.refreshControl?.endRefreshing()
+        super.viewWillAppear(animated)
+        
+        refresh()
+        tableView.setContentOffset(CGPoint(x: 0, y: -tableView.contentInset.top), animated: false)
+    }
+    
+    @IBAction func refresh() {
+        refreshControl?.beginRefreshing()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            if self.feed.isEmpty {
+                self.feed = FeedImageViewModel.prototypeFeed
+                self.tableView.reloadData()
             }
+            self.refreshControl?.endRefreshing()
         }
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return feed.count
@@ -56,30 +56,70 @@ extension FeedImageCell {
         descriptionLabel.text = model.description
         descriptionLabel.isHidden = model.description == nil
         
-        fadeIn(UIImage(named: model.imageName)) 
+        fadeIn(UIImage(named: model.imageName))
     }
     
     override func awakeFromNib() {
-            super.awakeFromNib()
+        super.awakeFromNib()
+        
+        feedImageView.alpha = 0
+        feedImageContainer.startShimmering()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        feedImageView.alpha = 0
+        feedImageContainer.startShimmering()
+    }
+    
+    func fadeIn(_ image: UIImage?) {
+        feedImageView.image = image
 
-            feedImageView.alpha = 0
-        }
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0.3,
+            usingSpringWithDamping: 0.25,
+            initialSpringVelocity: 1.25,
+            options: [],
+            animations: {
+                self.feedImageView.alpha = 1
+            }, completion: { completed in
+                if completed {
+                    self.feedImageContainer.stopShimmering()
+                }
+            })
+    }
+}
 
-        override func prepareForReuse() {
-            super.prepareForReuse()
-
-            feedImageView.alpha = 0
-        }
-
-        func fadeIn(_ image: UIImage?) {
-            feedImageView.image = image
-
-            UIView.animate(
-                withDuration: 0.3,
-                delay: 0.3,
-                options: [],
-                animations: {
-                    self.feedImageView.alpha = 1
-                })
-        }
+private extension UIView {
+    private var shimmerAnimationKey: String {
+        return "shimmer"
+    }
+    
+    func startShimmering() {
+        let white = UIColor.white.cgColor
+        let alpha = UIColor.white.withAlphaComponent(0.7).cgColor
+        let width = bounds.width
+        let height = bounds.height
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [alpha, white, alpha]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.4)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.6)
+        gradient.locations = [0.4, 0.5, 0.6]
+        gradient.frame = CGRect(x: -width, y: 0, width: width*3, height: height)
+        layer.mask = gradient
+        
+        let animation = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.locations))
+        animation.fromValue = [0.0, 0.1, 0.2]
+        animation.toValue = [0.8, 0.9, 1.0]
+        animation.duration = 1
+        animation.repeatCount = .infinity
+        gradient.add(animation, forKey: shimmerAnimationKey)
+    }
+    
+    func stopShimmering() {
+        layer.mask = nil
+    }
 }
