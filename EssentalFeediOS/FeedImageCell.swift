3cf1427be47b03ea6b4e41c70f766aacebf7bc1a
Betzer09 <betzer.austin09@gmail.com>
2022-06-20 15:50:18 -0600

    Rendered loaded feed on successful load completion

---
+++ EssentalFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentalFeediOS
+//
+//  Created by Austin Betzer on 6/20/22.
+//
+
+import Foundation
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
