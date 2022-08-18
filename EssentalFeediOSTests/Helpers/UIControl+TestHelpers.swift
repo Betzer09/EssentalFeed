b0a6e15fcec635c2ed3f590e6d64d3f5548f475e
Betzer09 <betzer.austin09@gmail.com>
2022-08-17 19:06:28 -0600

    Extract duplicate logic into a reusable UIControl extension

---
+++ EssentalFeediOSTests/Helpers/UIControl+TestHelpers.swift
@@ -0,0 +1,19 @@
+//
+//  UIControl+TestHelpers.swift
+//  EssentalFeediOSTests
+//
+//  Created by Austin Betzer on 8/17/22.
+//
+
+import UIKit
+
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
