8f6f2bff04a60e1f6a1d027801298da4a96e8690
Betzer09 <betzer.austin09@gmail.com>
2022-08-17 19:09:20 -0600

    Extract FeedImageCell test helpers to a separate file

---
+++ EssentalFeediOSTests/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,39 @@
+//
+//  FeedImageCell+TestHelpers.swift
+//  EssentalFeediOSTests
+//
+//  Created by Austin Betzer on 8/17/22.
+//
+
+import UIKit
+import EssentalFeediOS
+
+extension FeedImageCell {
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+    
+    var isShowingImageLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+    
+    var locationText: String? {
+        return locationLabel.text
+    }
+    
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+    
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+    
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+    
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+}
