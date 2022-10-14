82b7e6332c1600a5984188fe6cd20f044e02acdc
Betzer09 <betzer.austin09@gmail.com>
2022-10-14 14:36:52 -0600

    Add Presentable View Models as pure data to clarify communication between Presentation and UI.

--- EssentalFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentalFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import EssentalFeed
 
 final class FeedRefreshViewController: NSObject, FeedLoadingView {
+   
     private(set) lazy var view = loadView()
     
     private let presenter: FeedPresenter
@@ -21,8 +22,8 @@ final class FeedRefreshViewController: NSObject, FeedLoadingView {
         presenter.loadFeed()
     }
     
-    func display(isLoading: Bool) {
-        if isLoading {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        if viewModel.isLoading {
             view.beginRefreshing()
         } else {
             view.endRefreshing()
