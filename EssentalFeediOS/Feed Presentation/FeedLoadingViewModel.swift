ec713b4e71a5569f47e51167bf988c55c5198868
Betzer09 <betzer.austin09@gmail.com>
2022-10-14 15:01:02 -0600

    Move ViewModels to separate files

---
+++ EssentalFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentalFeediOS
+//
+//  Created by Austin Betzer on 10/14/22.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentalFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentalFeediOS
+//
+//  Created by Austin Betzer on 10/14/22.
+//
+
+import Foundation
+import EssentalFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
